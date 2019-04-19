dd194392de9fd0078ba42f09b07132e39fe0ec0b
Prayugo24 <dwi.deni24@gmail.com>
2019-04-20 01:00:14 +0700

    Version 1.0

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use App\Models\Example;
+
+class HomeController extends Controller
+{
+    public function index(){
+        return view('content/welcome_content');
+    }
+}
